f3f1f28846c4a93f22ed61f5cc82a3cdabac0ad8
Gabriel Salvador <gabrielps2@al.insper.edu.br>
2017-04-06 18:10:43 -0300

    programa completo

--- Game.Arquive.DataBase/Program_join.py
+++ Game.Arquive.DataBase/Program_join.py
@@ -1,34 +1,48 @@
 import random 
 import json
-
-
+import colorama
+from colorama import Fore, Back, Style, init
+colorama.init()
+init(autoreset=True)
+#Variaveis--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+list_player = []
+#Funções----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 def passear_ou_dormir(pergunta_inicial):
 	while True:
 		pergunta_inicial = input("Você quer passear ou dormir?:")
 		if pergunta_inicial == "dormir" or pergunta_inicial == "passear":
 			return pergunta_inicial
 		else:
-			print("Não existe essa função")
+			print(Back.MAGENTA+"Não existe essa função!")
 
 def inspermon_inicial(dados):
+	#list_player = []
 	while True:
-		escolha = input("Escolha entre os inspermons Boludosmon , Turtwig ou Pixu:")
-		if escolha == "Boludosmon":
+		#print("Escolha um dos seguintes Inspermons iniciais:")
+		print("Primeira Opção:\n{0}\nPoder:{1}\nVida:{2}\nDefesa:{3}".format(dados[6]['nome'],dados[6]['poder'],dados[6]['vida'],dados[6]['defesa']))
+		print("Segunda Opção:\n{0}\nPoder:{1}\nVida:{2}\nDefesa:{3}".format(dados[7]['nome'],dados[7]['poder'],dados[7]['vida'],dados[7]['defesa']))
+		print("Terceira Opção:\n{0}\nPoder:{1}\nVida:{2}\nDefesa:{3}".format(dados[11]['nome'],dados[11]['poder'],dados[11]['vida'],dados[11]['defesa']))
+		escolha = input("Escolha entre os Inspermons {0}, {1} ou {2}:".format(dados[6]["nome"],dados[7]["nome"],dados[11]["nome"]))
+		if escolha == dados[6]['nome']:
 			colection = dados[6] 
-			return "Você escolheu o Inspermon:Boludosmon" # so mostra o nome do Insper
+			list_player.append(dados[6])
+			print("Você escolheu o Inspermon: {0}".format(dados[6]['nome'])) # so mostra o nome do Insper
+			return (Fore.WHITE + "Inspermon Adicionado!")
 			break
-		elif escolha == "Turtwig":
+		elif escolha == dados[7]['nome']:
 			colection = dados[7]
-			return "Você escolheu o Inspermon:Turtwig"
+			print("Você escolheu o Inspermon: {0}".format(dados[7]['nome']))
+			list_player.append(dados[7])
+			return (Fore.WHITE + "Inspermon Adicionado!")
 			break
-		elif escolha == "Pixu":
-			colection = dados[8]
-			return "Você escolheu o Inspermon:Pixu"  
-			break
+		elif escolha == dados[11]['nome']:
+			colection = dados[11]
+			print("Você escolheu o Inspermon: {0}".format(dados[11]['nome']) ) 
+			list_player.append(dados[11])
+			return (Fore.WHITE + "Inspermon Adicionado!")
 		else:
-			print("Erro!Escolha novamente")
+			print(Style.DIM + "Erro! Escolha novamente(Porfavor copie exatamente como mostrado!)")
 			continue			
-
 def Aparecimento_de_Mons(dados):
 	lista_Mons = []
 	for e in dados:
@@ -40,16 +54,20 @@ def Aparecimento_de_Mons(dados):
 	return lista_Mons[key]
 #print (Aparecimento_de_Mons(dados))
 
-# Programa Principal
+# Programa Principal----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 with open("Data_Inspermons.json") as arquivo:
 	dados = json.load(arquivo)
-
+with open('texto de introduçao.txt','r') as texto:
+	for linha in texto.readlines():
+		print(Fore.GREEN+linha)
+print(inspermon_inicial(dados))
+#list_player.append(inspermon_inicial(dados))
+print("Inspermons membros o de seu time: {0}".format(list_player))
 
 pergunta_inicial = passear_ou_dormir("")
 
 if pergunta_inicial == "passear":
-	print("Vamos passear")
-	print(inspermon_inicial(dados))
-	print(Aparecimento_de_Mons(dados))
+	print(Fore.RED+"Passeando...")
+	print(Fore.RED+"Inspermon encontrado!\n {0}".format(Aparecimento_de_Mons(dados)))
 else:
-	print("Ate a proxima")
+	print(Fore.BLUE + "Ate a proxima!")
