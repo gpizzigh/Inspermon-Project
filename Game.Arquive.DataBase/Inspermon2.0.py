bee615ae703597eb9cf0091998ebd0f9bbb48aad
KLiuyeh <kevinl@al.insper.edu.br>
2017-04-24 10:36:41 -0300

    dano critico ,ajuste de erro ,

--- Game.Arquive.DataBase/Inspermon2.0.py
+++ Game.Arquive.DataBase/Inspermon2.0.py
@@ -9,7 +9,6 @@ list_player = []
 Insperdex = []
 #Funções----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 def passear_ou_dormir():
-    #pergunta_inicial = input("Você quer passear ou dormir?:")
 	f = True
 	while f:
 		pergunta_inicial = input("Você deseja passear, dormir ou acessar o Insperdex?: ")
@@ -23,15 +22,16 @@ def passear_ou_dormir():
 			print(Fore.RED+"Inspermon encontrado!\n")
 			print(Fore.BLACK + "------"*20)
 			batalha_2(dados)
-			#lvl_up(dados)
+			lvl_up(list_player) 
 		elif pergunta_inicial == "Insperdex":
 			print(Insperdex)
+			print(funt_Insperdex(Insperdex))
+			print(len(Insperdex))
 		else:
 			print(Back.MAGENTA+"Não existe esta função!")
-			#return pergunta_inicial # !!Funçao OK!!
+			
 
-def inspermon_inicial(dados):
-	#list_player = []
+def inspermon_inicial(dados):  #OK
 	while True:
 		print("Escolha um dos seguintes Inspermons iniciais:")
 		print(Fore.BLUE + "Primeira Opção:\n{0}\nPoder:{1}\nVida:{2}\nDefesa:{3}".format(dados[6]['nome'],dados[6]['poder'],dados[6]['vida'],dados[6]['defesa']))
@@ -41,7 +41,7 @@ def inspermon_inicial(dados):
 		if escolha == dados[6]['nome']:
 			colection = dados[6]
 			list_player.append(dados[6])
-
+			
 			print("Você escolheu o Inspermon: {0}".format(dados[6]['nome'])) # so mostra o nome do Insper
 			return (Fore.WHITE + "Inspermon Adicionado!")
 			break
@@ -50,20 +50,19 @@ def inspermon_inicial(dados):
 			print("Você escolheu o Inspermon: {0}".format(dados[7]['nome']))
 			list_player.append(dados[7])
 			return (Fore.WHITE + "Inspermon Adicionado!")
-
+			
 			break
 		elif escolha == dados[11]['nome']:
 			colection = dados[11]
 			print("Você escolheu o Inspermon: {0}".format(dados[11]['nome']))
 			list_player.append(dados[11])
-
 			return (Fore.WHITE + "Inspermon Adicionado!")
 			break
 		else:
 			print(Style.DIM + Fore.RED +"Erro! Escolha novamente(Porfavor copie exatamente como mostrado!)")
 			continue #funçao no estado OK
 
-def Aparecimento_de_Mons(dados):
+def Aparecimento_de_Mons(dados):  #OK
 	lista_Mons = []
 	for e in dados:
 		for x in e.keys():
@@ -74,42 +73,8 @@ def Aparecimento_de_Mons(dados):
 	return lista_Mons[key]
 #print (Aparecimento_de_Mons(dados))
 
-'''def batalha(dados):
-	n=0
-	list_0 = []
-	list_0.append(Aparecimento_de_Mons(dados))
-	#print(list_0[g]) #No Error
-	while n < 46:
-		if list_0[0] == dados[n]["nome"]:
-			dados_oponente = dados[n]
-		n=n+1
-	Insperdex.append(dados_oponente["nome"])
-	print(Fore.YELLOW + dados_oponente["nome"]) # No Error
-	vida_oponente = dados_oponente["vida"]
-	vida_jogador = list_player[g]["vida"]  # Dando problema . Problema apos perder algum Hp sem Morrer e apos isso ele morrre o hp Inspermon nao e restaurado 100%
-	fugir_ou_lutar = input("Você quer fugir ou lutar?:")
-	f = True
-	while f:
-		if fugir_ou_lutar == "fugir":
-			print("Você fugiu da batalha")
-			f = False
-		elif fugir_ou_lutar == "lutar":
-			print(Fore.RED + "Prepare-se para a batalha!")
-#--------------------------------------Teste---------------------------------------------------------------
-			print("Escolha um dos seguintes Inspermons de seu time para Batalhar: ")
-			x = 0
-			while x < len(list_player):
-				print("{0} - {1}".format(x,list_player[x]["nome"]))
-				x += 1
-			escolhido = int(input("Digite o numero de seu Inspermon escolhido: "))
-			for g in range(len(list_player)):
-				if escolhido == g:
-					print("Inspermon escolhido: {0} ".format(list_player[g]["nome"]))
-					vida_jogador = list_player[g]["vida"]
-					print(list_player[g]["vida"])
-#-----------------------------------------Teste------------------------------------------------------------
-			batalha_2(dados)'''
-def batalha_2(dados):
+
+def batalha_2(dados):  
 #----------------------------------------Teste-----------------------------------------
 	n=0
 	list_0 = []
@@ -122,37 +87,54 @@ def batalha_2(dados):
 	Insperdex.append(dados_oponente["nome"])
 	print(Fore.YELLOW + dados_oponente["nome"]) # No Error
 	vida_oponente = dados_oponente["vida"]
-	'''vida_jogador = list_player[g]["vida"]'''  # Dando problema . Problema apos perder algum Hp sem Morrer e apos isso ele morrre o hp Inspermon nao e restaurado 100%
-	fugir_ou_lutar = input("Você deseja fugir ou lutar?:")
+	
 	f = True
 	while f:
+		fugir_ou_lutar = input("Você deseja fugir ou lutar?:")
 		if fugir_ou_lutar == "fugir":
 			print("Você fugiu da batalha")
 			f = False
 		elif fugir_ou_lutar == "lutar":
 			print(Fore.RED + "Prepare-se para a batalha!")
-#----------------------------------------Teste----------------------------------------
+#--------------------------------------Teste---------------------------------------------------------------
 			print("Escolha um dos seguintes Inspermons de seu time para Batalhar: ")
 			x = 0
 			while x < len(list_player):
 				print("{0} - {1}".format(x,list_player[x]["nome"]))
 				x += 1
-			escolhido = int(input("Digite o numero de seu Inspermon escolhido: "))
+			
 			for g in range(len(list_player)):
+				escolhido = int(input("Digite o numero de seu Inspermon escolhido: "))
 				if escolhido == g:
 					print("Inspermon escolhido: {0} ".format(list_player[g]["nome"]))
 					vida_jogador = list_player[g]["vida"]
-					#print(list_player[g]["vida"]) # Sem Erro!
-#----------------------------------------Teste----------------------------------------
+					print(list_player[g]["vida"])
+					
+
+
 					while list_player[g]["vida"]>0 and dados_oponente["vida"]>0:
+						z = random.randint(0,10)
 						if list_player[g]["poder"] >= dados_oponente["defesa"]:
-							dados_oponente["vida"] = dados_oponente["vida"] - (list_player[g]["poder"] - dados_oponente["defesa"])
+							if z == 9:
+								dados_oponente["vida"] = dados_oponente["vida"] - (list_player[g]["poder"]*100 - dados_oponente["defesa"])
+								print("Ataque critico feito pelo Inspermon do jogador") 
+							else:
+								dados_oponente["vida"] = dados_oponente["vida"] - (list_player[g]["poder"] - dados_oponente["defesa"])
+
+						elif list_player[g]["poder"] <= dados_oponente["defesa"]:
+							if z == 9:
+								dados_oponente["vida"] = dados_oponente["vida"] - (list_player[g]["poder"]*100 - dados_oponente["defesa"])
+								print("Ataque critico do Inspermon do jogador") 
+							else:
+								dados_oponente["vida"] = dados_oponente["vida"]
+
+
 						if dados_oponente["vida"] <= 0:
 							list_player[g]["XP"] = list_player[g]["XP"] + 10
-							list_player[g]["vida"] = vida_jogador  # Coloquei somente para tentar resolver problema do HP da linha 85.
+							list_player[g]["vida"] = vida_jogador  
 							print(Fore.GREEN+Style.BRIGHT+'Vitoria! o Inspermon {0} foi derrotado com sucesso!'.format(dados_oponente["nome"]))
 							print('A experiência de seu Inspermon é agora de {0}\nA vida do seu Inspermon agora é de {1}'.format(list_player[g]["XP"],list_player[g]["vida"]))
-							#lvl_up(dados)
+
 							while True:
 								d = input("Voce deseja adiciona-lo ao seu time?(Sim ou Nao): ")
 								if d == "Sim":
@@ -168,7 +150,19 @@ def batalha_2(dados):
 								else:
 									print("Por favor Tente novamente (UTILIZE MAIUSCULAS!)")
 						if dados_oponente["poder"] > list_player[g]["defesa"]:
-							list_player[g]["vida"] = list_player[g]["vida"] - (dados_oponente["poder"] - list_player[g]["defesa"])
+							if z == 5:
+								list_player[g]["vida"] = list_player[g]["vida"] - (dados_oponente["poder"]*100 - list_player[g]["defesa"])
+								print("Ataque critico do oponente")
+							else:
+								list_player[g]["vida"] = list_player[g]["vida"] - (dados_oponente["poder"] - list_player[g]["defesa"])
+
+						elif dados_oponente["poder"] <= list_player[g]["defesa"]:
+							if z == 5:
+								list_player[g]["vida"] = list_player[g]["vida"] - (dados_oponente["poder"]*100 - list_player[g]["defesa"])
+								print("Ataque critico do oponente")
+							else:
+								list_player[g]["vida"] = list_player[g]["vida"]
+
 						if list_player[g]["vida"] <= 0:
 							print(Fore.RED + Style.BRIGHT +"Você corre em desespero, nervoso e assustado, com seu inspermon em seu colo, enquanto ele libera seu ultimo folego de vida...")
 							while True:
@@ -181,53 +175,62 @@ def batalha_2(dados):
 									return
 								else:
 									print("Nao reconheço este comando, porfavor tente novamente...:")
-								if list_player[g]["poder"] >= dados_oponente["defesa"]:
-									dados_oponente["vida"] = dados_oponente["vida"]
-								else:
-									dados_oponente["poder"] >= list_player[g]["defesa"]
+						
+				else:
+					print("Digite novamente!")  # Dando erro estranho
+					continue
+
 		else:
-			print(Fore.RED + "Porfavor digite novamente")  # Ainda esta dand erro aqui! ele nao esta retornando para o inicio do while porfavor resolver!
-			break
+		    print(Fore.RED + "Porfavor digite novamente")  # Arrumei o erro !
+		    continue
 
 
+def lvl_up(list_player):  # OK
+	for x in range(len(list_player)):
+		if list_player[x]["XP"] == 50:
+			list_player[x]["XP"]  = 55
+			list_player[x]["poder"] = list_player[x]["poder"] * 1.5
+			list_player[x]["vida"] = list_player[x]["vida"] * 1.7
+			list_player[x]["defesa"] = list_player[x]["defesa"] * 1.5
+			print("O seu Inspermon,{0} evoluiu.\nInspermon status:\npoder:{1}\ndefesa:{2}\nvida:{3}".format(list_player[x]["nome"],list_player[x]["poder"],list_player[x]["defesa"],list_player[x]["vida"]))
+			return list_player[x]
+		elif list_player[x]["XP"] == 155:
+			list_player[x]["XP"] == 160
+			list_player[x]["poder"] = list_player[x]["poder"] *1.6
+			list_player[x]["vida"] = list_player[x]["vida"] * 1.7
+			list_player[x]["defesa"] = list_player[x]["defesa"] * 1.6
+			print("O Inspermon,{0}, evoluiu.\nInspermon status:\npoder:{1}\ndefesa:{2}\nvida:{3}".format(list_player[x]["nome"],list_player[x]["poder"],list_player[x]["defesa"],list_player[x]["vida"]))
+			return list_player[x]
 
-def lvl_up(dados):  # Dando erro. Lista out of range
-    x = 0
-    while x <= len(list_player): # certo
-        if list_player[x]["XP"] == 50:
-            if list_player[x]["nome"] =="Gladium":
-                print("Seu Inspermou esta evoluindo!")
-                print("..."*5)
-                print("Parabens! Seu inspermom evoluiu de Gladium para Warirum!")
-                list_player[x] = dados[19]
- #------------------- Utilizar este formato de codigo para repetiçao! -------------
-            if list_player[x]["nome"] == "Oddibia":
-                print("Seu Inspermou esta evoluindo!")
-                print("..."*5)
-                print("Parabens! Seu inspermom evoluiu de Oddibia para Orgatos!")
-                list_player[x] = dados[21]
- #----------------------------------------------------------------------------------
-            elif list_player[x]["nome"] == "Moltander":
-                print("Seu Inspermou esta evoluindo!")
-                print("..."*5)
-                print("Parabens! Seu inspermom evoluiu de Moltander para Termeon!")
-                list_player[x] = dados[23]
-        x += 1
-def escolha(dados):
-	print("Escolha um dos seguintes Inspermons de seu time para Batalhar: ")
+
+def funt_Insperdex(Insperdex):  # Dando erro
+	
+	for y in range(len(Insperdex)):
+		if Insperdex[y] not in Insper2:
+			Insper2.append(Insperdex[y])
+			return Insper2
+
+def escolha(dados):  
+	print("Escolha um dos seguintes Inspermons de seu time para Batalhar:")
 	x = 0
 	while x < len(list_player):
 		print("{0} - {1}".format(x,list_player[x]["nome"]))
 		x += 1
-	escolhido = int(input("Digite o numero de seu Inspermon escolhido: "))
 	for g in range(len(list_player)):
+		escolhido = int(input("Digite o numero de seu Inspermon escolhido: "))
 		if escolhido == g:
 			print("Inspermon escolhido: {0} ".format(list_player[g]["nome"]))
 			vida_jogador = list_player[g]["vida"]
-
+		else:
+			print("Digite novamente")
+			continue
 
 # Programa Principal----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 # -------------- Texto -------------------------------------------------------------
+
+Insper2 = []
+
+
 with open("Data_Inspermons.json") as arquivo:
 	dados = json.load(arquivo)
 
@@ -239,9 +242,9 @@ with open('texto de introduçao.txt','r',encoding='Latin-1') as texto:
 print("------"*20)
 #-------------- Escolha do Inspermon inicial ---------------------------------------
 inspermon_inicial(dados)
-Insperdex.append(dados[6]['nome'])
-Insperdex.append(dados[7]['nome'])
-Insperdex.append(dados[11]['nome'])
+Insperdex.append(dados[6])
+Insperdex.append(dados[7])
+Insperdex.append(dados[11])
 print("Inspermons membros o de seu time: {0}".format(list_player))
 print("------"*20)
 # ------------- Funçao Passear ou dormir -------------------------------------------
